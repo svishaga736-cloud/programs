e8e1441a8fa1ef292bb4ad2d21562df4aade5c5c
svishaga736-cloud <svishaga736@gmail.com>
2026-03-02 21:16:06 +0530

    palindrome

---
+++ palindrome.py
@@ -0,0 +1,24 @@
+def longestPalindrome(s: str) -> str:
+    res = ""
+    res_len = 0
+
+    for i in range(len(s)):
+        # Check for odd length palindromes (center at i)
+        l, r = i, i
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            if (r - l + 1) > res_len:
+                res = s[l : r + 1]
+                res_len = r - l + 1
+            l -= 1
+            r += 1
+
+        # Check for even length palindromes (center between i and i+1)
+        l, r = i, i + 1
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            if (r - l + 1) > res_len:
+                res = s[l : r + 1]
+                res_len = r - l + 1
+            l -= 1
+            r += 1
+            
+    return res
